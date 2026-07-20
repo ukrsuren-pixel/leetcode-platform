09435b534e8226febb8a4140dc01601bb674ee9a
ukrsuren-pixel <ukrsuren@gmail.com>
2026-07-20 14:08:46 +0530

    Create 509.Fibonacci Number.java

---
+++ 509.Fibonacci Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fib(int n) {
+
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        int first = 0;
+        int second = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        return second;
+    }
+}
