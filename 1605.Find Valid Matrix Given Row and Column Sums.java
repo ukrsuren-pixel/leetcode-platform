1e1403d7c13064a4ee4abfed279d2c1c88ee84b0
ukrsuren-pixel <ukrsuren@gmail.com>
2026-07-21 09:04:49 +0530

    Create 1605.Find Valid Matrix Given Row and Column Sums.java

---
+++ 1605.Find Valid Matrix Given Row and Column Sums.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+
+        int m = rowSum.length;
+        int n = colSum.length;
+
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                int value = Math.min(rowSum[i], colSum[j]);
+
+                matrix[i][j] = value;
+
+                rowSum[i] -= value;
+                colSum[j] -= value;
+            }
+        }
+
+        return matrix;
+    }
+}
