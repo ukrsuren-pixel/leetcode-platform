41d72b0ea76c6a0595bf995ac41c3cb0020fec15
ukrsuren-pixel <ukrsuren@gmail.com>
2026-07-23 09:40:53 +0530

    Create 709.To Lower Case.java

---
+++ 709.To Lower Case.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String toLowerCase(String s) {
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch + 32);
+            }
+
+            ans.append(ch);
+        }
+
+        return ans.toString();
+    }
+}
