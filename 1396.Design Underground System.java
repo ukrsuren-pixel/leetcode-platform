1c6acc6ccdde1405c50d85dc655be2f9c69dc024
R.Surenthara Kumar <ukrsuren@gmail.com>
2026-07-24 10:34:24 +0530

    Create 1396.Design Underground System.java

---
+++ 1396.Design Underground System.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class UndergroundSystem {
+
+    class CheckIn {
+        String station;
+        int time;
+
+        CheckIn(String station, int time) {
+            this.station = station;
+            this.time = time;
+        }
+    }
+
+    class Trip {
+        int totalTime;
+        int count;
+
+        Trip() {
+            totalTime = 0;
+            count = 0;
+        }
+    }
+
+    HashMap<Integer, CheckIn> checkInMap;
+    HashMap<String, Trip> tripMap;
+
+    public UndergroundSystem() {
+        checkInMap = new HashMap<>();
+        tripMap = new HashMap<>();
+    }
+
+    public void checkIn(int id, String stationName, int t) {
+        checkInMap.put(id, new CheckIn(stationName, t));
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+
+        CheckIn in = checkInMap.get(id);
+
+        String route = in.station + "->" + stationName;
+
+        int travelTime = t - in.time;
+
+        Trip trip = tripMap.getOrDefault(route, new Trip());
+
+        trip.totalTime += travelTime;
+        trip.count++;
+
+        tripMap.put(route, trip);
+
+        checkInMap.remove(id);
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+
+        String route = startStation + "->" + endStation;
+
+        Trip trip = tripMap.get(route);
+
+        return (double) trip.totalTime / trip.count;
+    }
+}
